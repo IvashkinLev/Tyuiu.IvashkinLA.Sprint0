77430dcd03d7f9af7089a4cf0b5244fb935d53da
mordvinych <mordvinychofficial@gmail.com>
2026-09-13 15:55:30 +0500

    Task2

---
+++ Tyuiu.IvashkinLA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.IvashkinLA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.IvashkinLA.Sprint0.Task2.V0.Test
+{
+    public class DataServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckGetMessageValid()
+        {
+            var name = "Лев";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual($"Привет, {name}!", res);
+        }
+    }
+}
---
+++ Tyuiu.IvashkinLA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.IvashkinLA.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Лев"));
+Console.ReadKey();
