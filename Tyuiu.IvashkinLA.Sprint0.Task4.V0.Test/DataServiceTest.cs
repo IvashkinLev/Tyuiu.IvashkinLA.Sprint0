6010b05797fdf2a24a7553566944a388be80ac52
mordvinych <mordvinychofficial@gmail.com>
2026-09-13 19:20:16 +0500

    Task4

---
+++ Tyuiu.IvashkinLA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+using Tyuiu.IvashkinLA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.IvashkinLA.Sprint0.Task4.V0.Test
+{
+    public class DataServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckedAdditionValid()
+        {
+            Assert.That(DataService.Addition(5, 5), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void CheckedSubstractionValid()
+        {
+            Assert.That(DataService.Substraction(10, 5), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.That(DataService.Multiplication(5, 5), Is.EqualTo(25));
+        }
+
+        [Test]
+        public void CheckedDivisionValid()
+        {
+            Assert.That(DataService.Division(10, 5), Is.EqualTo(2));
+        }
+    }
+}
---
+++ Tyuiu.IvashkinLA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.IvashkinLA.Sprint0.Task4.V0.Lib;
+
+Console.WriteLine(DataService.Addition(5, 5));
+Console.WriteLine(DataService.Substraction(10, 5));
+Console.WriteLine(DataService.Multiplication(5, 5));
+Console.WriteLine(DataService.Division(10, 5));
+
+Console.ReadKey();
